40b7783d82f43b5ec88d5b43d870ead98c2afa96
diegorezm <dmourarezende@gmail.com>
2023-10-20 16:08:14 -0300

    rm dist

--- api/dist/controllers/HomeController.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  async index(req , res){
-    return res.json({
-      message:"There is nothing to be done here!"
-    });
-  }
-}
-exports. default = new HomeController();
